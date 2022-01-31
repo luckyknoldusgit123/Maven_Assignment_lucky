e155b1b094ecf2ef1464cd52bf1afd254af7f7be
luckyknoldusgit123 <lucky.kumar@knoldus.com>
2022-01-31 17:32:32 +0530

    Initial commit

---
+++ assign/cal-impl/src/main/java/Application.java
@@ -0,0 +1,14 @@
+
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+        cal.add(1,2);
+        System.out.println(cal.add(1,2));
+
+    }
+}
+
+
